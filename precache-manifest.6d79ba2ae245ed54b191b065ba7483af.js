12c9212a60ea095b4704175f3a97335cb33647c2
hidayatkhan <hidayat.rehman64@gmail.com>
2020-02-07 11:29:12 +0500

    Updates

---
+++ precache-manifest.6d79ba2ae245ed54b191b065ba7483af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e8e1357d9c5cbaebc5735ff10e0429d",
+    "url": "/todoApp/index.html"
+  },
+  {
+    "revision": "ea9da2a43fed63b61df2",
+    "url": "/todoApp/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "1aea80a03917f8f1fdec",
+    "url": "/todoApp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ea9da2a43fed63b61df2",
+    "url": "/todoApp/static/js/2.dbc00615.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todoApp/static/js/2.dbc00615.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aea80a03917f8f1fdec",
+    "url": "/todoApp/static/js/main.8d926ad2.chunk.js"
+  },
+  {
+    "revision": "5d60c23da10519b01395",
+    "url": "/todoApp/static/js/runtime-main.5ac05902.js"
+  }
+]);
